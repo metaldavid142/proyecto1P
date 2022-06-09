d0e5db398c83b20781a41e964e12055abd195e49
metaldavid <mdsangucho@espe.edu.ec>
2022-06-09 04:09:17 -0500

    detalles

--- aplicacion.py
+++ aplicacion.py
@@ -1,11 +1,22 @@
 #Se importa la biblioteca de Flask
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for, flash
 
+#Objeto para inicializar la aplicación
+#1. Nombre por defecto
+#2. Ruta donde están los templates
 app = Flask(__name__, template_folder='templates')
 
+#Seguridad de la app
+app.secret_key = '0000'
+
+#Almacenamiento de tareas
+datos_formulario = []
+
+#Primer controlador: inicial
+#Lista actual de tareas pendientes y Formulario HTML para ingresar nuevas tareas
 @app.route('/')
 def principal():
-    return render_template('principal.html')
+    return render_template('principal.html', datos_formulario=datos_formulario)
 
 #Ruta de página 1
 @app.route('/drones')
@@ -31,6 +42,41 @@ def dron3():
 @app.route('/aeromodelismo')
 def aeromodelismo():
     return render_template('/aeromodelismo.html')
+
+#Controlador para el envío de un nuevo elemento
+@app.route('/enviar', methods=['POST'])
+def enviar():
+    if request.method == 'POST':
+        
+        #Campos a llenar
+        nombres = request.form['nombres']
+        celular = request.form['celular']
+        correo_electronico = request.form['correo_electronico']
+
+        #Validación para que los campos no sean nulos
+        if nombres == '' or correo_electronico == '':
+            flash('LLenar todos los campos')
+            return redirect(url_for('principal'))
+        
+        else:
+            flash('¡Tarea agregada exitosamente!')
+            datos_formulario.append({'nombres': nombres, 'celular': celular, 'correo_electronico': correo_electronico})
+            return redirect(url_for('principal'))
+
+#Controlador: Limpiar formulario
+@app.route('/borrar', methods=['POST'])
+def borrar():
+    if request.method == 'POST':
+        
+        #Verificación de datos
+        if datos_formulario == []:
+            flash('Formulario vacío')
+            return redirect(url_for('principal'))
+
+        else:
+            datos_formulario.clear()
+            flash('Formulario borrado')
+            return redirect(url_for('principal'))
 
 #Método para correr la aplicación
 if __name__ == '__main__':
